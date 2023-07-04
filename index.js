a013988d6fa9d936c40c9dd0f2763462f16e56a2
Priya Gupta <priya63874991@gmail.com>
2023-07-04 13:31:31 +0530

    index.js

--- index.js
+++ index.js
@@ -4,40 +4,69 @@ const path= require("path");
 const bodyParser = require('body-parser')
 const flash = require("flash");
 const session = require("express-session");
-// app.use("body-parser")
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+const database = require("./config/database");
+const {cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
+
+
 app.use(express.static('public'));
 app.set("view-engine" , "ejs");
-const port = process.env.PORT || 3000;
 
-const mongoose = require('mongoose');
+//PORT
+const port = process.env.PORT || 4000;
 
-mongoose.connect("mongodb://0.0.0.0/UserAPI",{
+//database connection
+database.connect();
 
-}).then(() =>{
-    console.log("connection successfull");
-}).catch(()=>{
-    console.log("no connection");
-});
+app.use(
+	fileUpload({
+		useTempFiles:true,
+		tempFileDir:"/tmp",
+	})
+)
+//cloudinary connection
+cloudinaryConnect();
 
+//routes
 const homeroutes = require("./routes/home")
 app.use("/", homeroutes);
-
-const Users = require('./Models/Users');
 
 const signuproutes = require("./routes/signup");
 app.use("/signup",signuproutes);
 
-app.post("/signup",Users);
-
 const loginrouts = require("./routes/login");
 app.use("/login",loginrouts);
 
-const paymentrouts = require("./routes/paymentSuccessEmail");
-app.use("/paymentSuccessEmail",paymentrouts);
+const createCourse= require("./routes/createCourse");
+app.use("/course",createCourse);
+
+const createCategory= require("./routes/createCategory");
+app.use("/category",createCategory);
+
+const courseDetails= require("./routes/showCourse");
+app.use("/courses",courseDetails);
+
+const paymentroutes = require("./routes/Payment");
+app.use("/payment",paymentroutes);
+
+const ProfileInfo = require("./routes/profileInfo");
+app.use("/profileInfo",ProfileInfo);
+
+const enrolledCoursesRoutes = require("./routes/enrolledCourses");
+app.use("enrollCourses",enrolledCoursesRoutes);
+
+const instructorCoursesRoutes = require("./routes/instructorCourses");
+app.use("enrollCourses",instructorCoursesRoutes);
+
+const rating = require("./routes/RatingAndReview");
+app.use('/',rating);
+
 
 app.listen(port,()=>{
     console.log(`connection is setup at ${port}`);
